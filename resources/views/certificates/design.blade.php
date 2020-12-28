ac330513761a0d586a784e7078642c871ed320a7
ikkerxd <henrry.saa@gmail.com>
2020-12-28 14:45:31 -0500

    correcion de certficado

--- resources/views/certificates/design.blade.php
+++ resources/views/certificates/design.blade.php
@@ -7,12 +7,9 @@
         html {
             margin: 0;
         }
-        .user, .dni, .curso, .fecha, .codigo, .estado {
+        .user, .dni, .curso, .fecha, .codigo, .estado, .horas {
             position: absolute;
-            margin-left: auto;
-            margin-right: auto;
-            left: 0;
-            right: 0;
+            left: 10px;
             height: auto;
             width: 70%;
             color: #373435;
@@ -20,24 +17,24 @@
         .title {
             font-family: Calibri,sans-serif;
             text-transform: uppercase;
-            text-align: center;
 
         }
 
         .user {
             font-family: serif;
             text-transform: uppercase;
-            text-align: center;
             top: 260px;
-            left: 15%;
-            font-size: 40px;
+            left: 8%;
+            width: 84%;
+            font-size: 34px;
             font-weight: 600;
+            color: #83141B;
         }
 
         .dni {
             font-family: Calibri,sans-serif;
-            top: 340px;
-            left: 62%;
+            top: 345px;
+            left: 27%;
             width: 25%;
             font-size: 28px;
             font-weight: 600;
@@ -45,43 +42,49 @@
 
         .estado {
             top: 380px;
-            left: 15%;
+            left: 8%;
             font-size: 24px;
             text-transform: capitalize !important;
         }
 
         .curso {
-            top: 425px;
+            top: 430px;
             left: 8%;
-            width: 84%;
+            width: 60%;
             font-weight: bold;
-            font-size: 40px;
+            font-size: 32px;
+            color: #83141B;
+        }
+
+        .horas {
+            font-family: sans-serif;
+            top: 580px;
+            left: 8%;
+            color: #3c3c3c;
+            font-size: 18px;
         }
 
         .fecha {
             font-family: sans-serif;
-            top: 730px;
-            left: 60px;
-            color: #373435;
-            font-size: 15px;
+            top: 600px;
+            left: 8%;
+            color: #3c3c3c;
+            font-size: 17px;
         }
 
         .codigo {
             font-family: sans-serif;
-            top: 90px;
-            left: 90%;
+            top: 10px;
+            left: 8%;
             font-weight: bold;
+            color: #83141B;
         }
     </style>
 
 </head>
 <body>
     <div style="position: fixed; left: 0; top: -265px; right: 0; bottom: 0px; text-align: center;z-index: -1000;">
-        @if($inscription->participant->company_id == 2)
-            <img src="https://www.ighgroup.com/campusperurail/img/certificates/CERTIFICADO-PERURAIL1.png" style="width: 100%; margin-top: 25%;">
-        @else
-            <img src="https://www.ighgroup.com/campusperurail/img/certificates/CERTIFICADO-TRANS1.png" style="width: 100%; margin-top: 25%;">
-        @endif
+        <img src="https://www.ighgroup.com/campusperurail/img/certificates/certi.jpg" style="width: 100%; margin-top: 25%;">
     </div>
     <spam class="codigo">{{ $inscription->id }}</spam>
     <spam class="user">{{ $inscription->participant->full_name }}</spam>
@@ -90,10 +93,10 @@
         <spam class="title estado">por haber <spam style="text-transform: uppercase !important; font-weight: bold">participado en el curso</spam></spam>
     @else
         <spam class="title estado">por haber <spam style="text-transform: uppercase !important; font-weight: bold">aprobado el curso</spam></spam>
-
     @endif
     <spam class="title curso">{{ $inscription->classroom->name }}</spam>
-    <spam class="fecha">{{ $inscription->classroom->startDatetimeLong() }}</spam>
-    <img src="data:image/svg+xml;base64,{{ base64_encode($codeQR) }}" style=" position: absolute; top: 672px; right: 120px; margin: 0; padding: 0">
+    <spam class="horas">Con una duración de 02 horas lectivas.</spam>
+    <spam class="fecha">El dia {{ $inscription->classroom->startDatetimeLong() }}</spam>
+    <img src="data:image/svg+xml;base64,{{ base64_encode($codeQR) }}" style=" position: absolute; top: 350px; right: 265px; margin: 0; padding: 0">
 </body>
 </html>
